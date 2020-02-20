1dd18932f0881c59802f204da5c48cd1799b91be
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-02-20 14:56:43 -0700

    delete functionality from local storage

--- app.js
+++ app.js
@@ -40,6 +40,16 @@ const StorageCtrl = (function() {
                 }
             });
             localStorage.setItem("items", JSON.stringify(items));
+        },
+        deleteItemFromStorage: function(id) {
+            let items = JSON.parse(localStorage.getItem("items"));
+
+            items.forEach(function(item, index) {
+                if(id === item.id) {
+                    items.splice(index, 1);
+                }
+            });
+            localStorage.setItem("items", JSON.stringify(items));
         }
     }
 })();
@@ -415,6 +425,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
+
+        // Delete from local storage
+        StorageCtrl.deleteItemFromStorage(currentItem.id);
 
         UICtrl.clearEditState();
 
