19c56b40d7441f24761f2345c8685c6b6f7cceec
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-02-20 14:51:52 -0700

    added update local storage functionality and added to UI

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ const StorageCtrl = (function() {
                 items = JSON.parse(localStorage.getItem("items"));
             }
             return items;
+        },
+        updateItemStorage: function(updatedItem) {
+            let items = JSON.parse(localStorage.getItem("items"));
+
+            items.forEach(function(item, index) {
+                if(updatedItem.id === item.id) {
+                    items.splice(index, 1, updatedItem);
+                }
+            });
+            localStorage.setItem("items", JSON.stringify(items));
         }
     }
 })();
@@ -377,6 +387,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
+
+        // Update local storage
+        StorageCtrl.updateItemStorage(updatedItem);
 
         UICtrl.clearEditState();
 
